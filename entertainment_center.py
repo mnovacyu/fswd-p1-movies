14e2d8acc1286e714304ca1b2be1640af8f1deca
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-01-04 14:12:39 -0800

    code review changes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,30 +1,57 @@
 import fresh_tomatoes
 import media
 
-# creating instances of media.Movie
-avengers = media.Movie("The Avengers", "Heroes get together.",
-                        "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-                        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
-kung_fu_hustle = media.Movie("Kung Fu Hustle", "A hooligan becomes something more.",
-                     "https://upload.wikimedia.org/wikipedia/en/0/00/KungFuHustleHKposter.jpg",
-                     "https://www.youtube.com/watch?v=-m3IB7N_PRk")
+# Avengers movie: movie title, storyline, poster image and movie trailer
+avengers = media.Movie(
+    "The Avengers",
+    "Heroes get together.",
+    "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",  #NOQA
+    "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
-memento = media.Movie("Memento", "A person tries to remember.",
-                      "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg",
-                      "https://www.youtube.com/watch?v=0vS0E9bBSL0")
+# Kung Fu Hustle movie: movie title, storyline, poster image and movie trailer
+kung_fu_hustle = media.Movie(
+    "Kung Fu Hustle",
+    "A hooligan becomes something more.",
+    "https://upload.wikimedia.org/wikipedia/en/0/00/KungFuHustleHKposter.jpg",  #NOQA
+    "https://www.youtube.com/watch?v=-m3IB7N_PRk")
 
-inception = media.Movie("Inception", "We have to go deeper.",
-                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+# Memento movie: movie title, storyline, poster image and movie trailer
+memento = media.Movie(
+    "Memento",
+    "A person tries to remember.",
+    "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg",
+    "https://www.youtube.com/watch?v=0vS0E9bBSL0")
 
-power_rangers = media.Movie("Power Rangers", "Teenagers with attitudes.",
-                            "https://upload.wikimedia.org/wikipedia/en/7/78/Power_Rangers_%282017_Official_Theatrical_Poster%29.png",
-                            "https://www.youtube.com/watch?v=5kIe6UZHSXw")
+# Inception movie: movie title, storyline, poster image and movie trailer
+inception = media.Movie(
+	"Inception",
+	"We have to go deeper.",
+	"https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  #NOQA
+	"https://www.youtube.com/watch?v=YoHD9XEInc0")
 
-inside_out = media.Movie("Inside Out", "Emotions come to life.",
-                         "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",
-                         "https://www.youtube.com/watch?v=seMwpP0yeu4")
+# Power Rangers movie: movie title, storyline, poster image and movie trailer
+power_rangers = media.Movie(
+	"Power Rangers",
+	"Teenagers with attitudes.",
+	"https://upload.wikimedia.org/wikipedia/en/7/78/Power_Rangers_%282017_Official_Theatrical_Poster%29.png",  #NOQA
+    "https://www.youtube.com/watch?v=5kIe6UZHSXw")
 
-# combining all instances into a movies array
-movies = [avengers, kung_fu_hustle, memento, inception, power_rangers, inside_out]
+# Inside Out movie: movie title, storyline, poster image and movie trailer
+inside_out = media.Movie(
+	"Inside Out",
+	"Emotions come to life.",
+	"https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",  #NOQA
+	"https://www.youtube.com/watch?v=seMwpP0yeu4")
+
+# Combining all instances into a movies array
+movies = [
+    avengers,
+    kung_fu_hustle,
+    memento,
+    inception,
+    power_rangers,
+    inside_out
+    ]
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,15 +1,20 @@
 import webbrowser
 
+
 class Movie():
-    """This class provides a way to store movie related information."""
+    """ This class provides a way to store movie related information. """
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """ This constructor method sets the parameters defined for a movie object """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ This function will open a web browser and go to the movie's YouTube trailer url """
         webbrowser.open(self.trailer_youtube_url)
+
